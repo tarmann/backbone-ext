33125051c111b6453f3dd99981eb19e21b02de66
Bruno Tarmann <tarmann@gmail.com>
2014-06-26 17:13:55 +0100

    update folder structure

---
+++ src/CollectionView.js
@@ -0,0 +1,39 @@
+var CollectionView = {
+
+  render: function(options){
+    _.each(this.collection, function(model){
+      this.renderItemView(model);
+    }, this);
+
+    return this;
+  },
+
+  renderItemView: function(model, options){
+    var viewData  = _.extend({}, { model: model }, this.options.itemViewOptions),
+        view      = new this.options.itemView(viewData);
+    
+    this.$el.append( view.render().el );
+
+    return this;
+  },
+
+	renderEmptyView: function(options){
+    if(! this.options.emptyView) return this;
+
+    this.emptyView = new this.getEmptyView(options);
+
+    this.$el.html( this.emptyView.render().el );
+
+    return this;
+	},
+
+  getEmptyView: function(){
+    return this.options.emptyView;
+  },
+
+  clearEl: function(){
+    this.$el.html('');
+    return this;
+  }
+
+};
---
+++ src/views/ItemView.js
@@ -0,0 +1,97 @@
+var BaseView = {
+
+  // Default viewData 
+  _viewData: {},
+
+  // Store all nested resources (models, collections)
+  _resources: {},
+
+  // Store all nested views 
+  _childViews: {},
+
+  // Get viewData object to be used to render view. The view data includes
+  // the current model binded to the view, model resources attached to the view
+  // and custom data provided on options.
+  getViewData: function(options){
+    var viewData = this._viewData || {};
+    
+    if(this.model){
+      viewData = _.extend(viewData, this.model.toJSON());
+    }
+
+    if(options){
+      viewData = _.extend(viewData, options);
+    }
+
+    // TODO: include all resources available
+
+    return viewData;
+  },
+
+  // Render current view using provided template.
+  // Takes viewData and silent as options.
+  // Trigger the following events: render:before, render:after, render.
+  render: function(addOptions){
+    if(!this.template) return this;
+
+    var options = _.extend({}, {
+      silent    : false,
+      viewData  : {}
+    }, addOptions);
+
+    if(options.silent) this.trigger('render:before', options);
+
+    this._beforeRender(options);
+
+    this.$el.html( this.template( this.getViewData(options.viewData) ));
+
+    this._afterRender(options);
+
+    if(options.silent) this.trigger('render', options);
+    if(options.silent) this.trigger('render:after', options);
+  },
+
+  // Render single element inside 
+  renderEl: function(el, options){
+    return this;
+  },
+
+  // Placeholder method executed before view is rendered
+  _beforeRender: function(options){
+    return this;
+  },
+
+  // Placeholder method executed after view is rendered
+  _afterRender: function(options){
+    return this;
+  },  
+
+  // Parse content of input fields into a object that will be used
+  // as a hash to be saved on the model binded to the view.
+  // TODO: move to form mixin
+  parseForm: function(){
+    // TODO: parseForm
+    return this.afterParseForm(formData);
+  },
+
+  // Custom parse the hash from parseForm method. 
+  // TODO: move to form mixin
+  afterParseForm: function(formData){
+    return formData;
+	},
+
+  // Parse form and save content on model binded to view.
+  // TODO: move to form mixin
+  save: function(options){
+    if(!this.model) return;
+    this.model.save( this.parseForm() );
+  },
+
+  // Remove view
+  close: function(){
+    this.trigger('close', this);
+    this.remove();
+  }
+
+};
+
